332189eec80d24906c4a4844c38417750b947498
root <root@8669616e5e3b>
2020-08-09 22:57:57 +0000

    1

--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -28,33 +28,44 @@ public class PhotoQuiz {
 		// “Copy Image Address”)
 
 		// 2. create a variable of type "Component" that will hold your image
-
+		Component image;
 		// 3. use the "createImage()" method below to initialize your Component
-
+		image = createImage("https://image.freepik.com/free-vector/vector-cartoon-cute-dog-wearing-hygienic-mask_52569-895.jpg");
 		// 4. add the image to the quiz window
-
+		quizWindow.add(image);
 		// 5. call the pack() method on the quiz window
-
+		quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+		String wrong = JOptionPane.showInputDialog("What is wrong with this dog? (*not the mask*)");
 		// 7. print "CORRECT" if the user gave the right answer
-
+		if(wrong.contains("thumb")) {
+			JOptionPane.showMessageDialog(null, "CORRECT");
+		}
 		// 8. print "INCORRECT" if the answer is wrong
-
+		else{
+			JOptionPane.showMessageDialog(null, "INCORRECT");
+		}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
-
+		quizWindow.remove(image);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+		Component image2;
+		image2 = createImage("https://previews.123rf.com/images/irwanjos/irwanjos1405/irwanjos140500009/28297808-funny-cat-cartoon.jpg");
 		// 11. add the second image to the quiz window
-
+		quizWindow.add(image2);
 		// 12. pack the quiz window
-
+		quizWindow.pack();
 		// 13. ask another question
-
+		String right = JOptionPane.showInputDialog("What color are the cats eyes??");
 		// 14+ check answer, say if correct or incorrect, etc.
-
+		if(right.contains("blue")) {
+			JOptionPane.showMessageDialog(null, "CORRECT");
+		}
+		
+		else {
+			JOptionPane.showMessageDialog(null, "INCORRECT");
+		}
 	}
 
 	private Component createImage(String imageUrl) throws MalformedURLException {
